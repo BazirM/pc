36f78e3bee84669a36cacd428ff465f94c83b192
BazirM <a75890@alunos.uminho.pt>
2018-05-23 23:34:39 +0100

    Add files via upload

---
+++ 2305/Player.java
@@ -0,0 +1,44 @@
+
+import java.lang.*;
+
+public class Player implements Comparable<Player>{
+    private String username;
+    private double score;
+    
+    Player(String user,double score){
+      username = user;
+      this.score = score;
+    }
+    
+    public String getUsername(){
+    return username;
+    }
+    
+    public void setScore(double score){
+      this.score = score;
+    }
+    
+    public double getScore(){
+      return score;
+    }
+    
+    public double[] getAtributes(){
+      double[] p= {score};
+      return p;
+    }
+    
+    public String toString(){
+      //return "Player: " + username + " score: " + score +"\n";
+      return " "+username+" "+score;
+    }
+    
+    public int compareTo(Player p){
+      synchronized(p){
+        if(score > p.getScore()) return -1;
+        else if(score < p.getScore()) return 1;
+        else return 0;
+      }
+    }
+    
+    
+}
